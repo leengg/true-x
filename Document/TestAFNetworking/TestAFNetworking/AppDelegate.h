42225d414898ae26f3571831048ad5611fc2e2c1
ngldchm <ngldchm@gmail.com>
2013-05-23 22:35:24 +0700

    Dao - add AFNetworking + TestAFNetworking

---
+++ Document/TestAFNetworking/TestAFNetworking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAFNetworking
+//
+//  Created by Dao Nguyen on 5/23/13.
+//  Copyright (c) 2013 Dao Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Document/TestAFNetworking/TestAFNetworking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestAFNetworking
+//
+//  Created by Dao Nguyen on 5/23/13.
+//  Copyright (c) 2013 Dao Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
